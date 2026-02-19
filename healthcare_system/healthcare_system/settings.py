6c9d60e2649a1ae7929ef9d552922e21183e25b4
Nimesh-019 <nimeshkhandhar@gmail.com>
2026-02-19 09:41:31 +0530

    [IMP] Database updated by Mysqlclient and mysql

--- healthcare_system/healthcare_system/settings.py
+++ healthcare_system/healthcare_system/settings.py
@@ -79,14 +79,20 @@ WSGI_APPLICATION = 'healthcare_system.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'healthcare_db',
+#         'USER': 'root',
+#         'PASSWORD': 'Kmp@2007',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'healthcare_db',
-        'USER': 'root',
-        'PASSWORD': 'Kmp@2007',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
